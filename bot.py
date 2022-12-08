e213b4029b95af4908a0bc1004396096ca8d3c52
Almir Paulo <ap.freelas@gmail.com>
2022-12-08 01:46:36 -0300

    correct

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_mail import Mail, Message
-import time, random
+import time, random, config
 
 app = Flask(__name__)
 mail = Mail()
@@ -12,7 +12,7 @@ msgs = ["blabla", "bleble","bbloblbo", "bleoblei"]
 @app.route('/', methods=['POST'])
 def email():
     while True:
-        time.sleep(15)
+        time.sleep(14)
         reminder = random.choice(msgs)
         print(reminder)
         #Getting data
@@ -34,16 +34,11 @@ def email():
 
 #Factorization
 #Configuration
-app.config['SECRET_KEY'] = '...'
-app.config['MAIL_SERVER'] = config.MAIL_SERVER
-app.config['MAIL_PORT'] = config.MAIL_PORT
 app.config['MAIL_USE_TLS'] = config.MAIL_USE_TLS
 app.config['MAIL_USE_SSL'] = config.MAIL_USE_SSL
 app.config['MAIL_USERNAME'] = config.MAIL_USERNAME
 app.config['MAIL_PASSWORD'] = config.MAIL_PASSWORD
 app.config['MAIL_DEFAULT_SENDER'] = config.MAIL_DEFAULT_SENDER
-app.config['MAIL_MAX_EMAILS'] = config.MAIL_MAX_EMAILS
-app.config['MAIL_ASCII_ATTACHMENTS'] = config.MAIL_ASCII_ATTACHMENTS
 
 #Initializations
 mail.init_app(app) 
